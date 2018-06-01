8155dcbe242201efd8241893a3aec2fdb97b7ba7
Alejandro Anzures <alejandreyes36@gmail.com>
2018-06-01 05:27:43 -0500

    Generar Reporte

--- src/app/componentes/casos-uso/agregar-caso/agregar-caso.component.ts
+++ src/app/componentes/casos-uso/agregar-caso/agregar-caso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
-
+import {FlujoEvento} from '../../../modelos/FlujoEventos';
 import { CasoUso } from '../../../modelos/casouso.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CasosUsoService } from '../../../servicios/casos-uso.service';
@@ -14,15 +14,34 @@ export class AgregarCasoComponent implements OnInit {
   chipsActions = new EventEmitter<string | MaterializeAction>();
   precondiciones: string[] = [];
   postcondiciones: string[] = [];
-  flujos: string[] = [];
-  eventos: string[] = [];
+
+  modalActions = new EventEmitter<string | MaterializeAction>();
+
+
+  //acciones = ["Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada","Mexico", "Canada"];
+  //acciones: string[];
   requisitosEspeciales: string[] = [];
   actores: string[] = [];
   chipsPlaceholder = {
     placeholder: '+Ingresa Campo',
     secondaryPlaceholder: 'Ingresa un nuevo campo',
   };
-  
+
+
+  provincias: string[] = [
+    'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 'Barcelona',
+    'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Córdoba',
+    'La Coruña', 'Cuenca', 'Gerona', 'Granada', 'Guadalajara',
+    'Guipúzcoa', 'Huelva', 'Huesca', 'IslasBaleares', 'Jaén', 'León', 'Lérida', 'Lugo',
+    'Madrid', 'Málaga', 'Murcia', 'Navarra', 'Orense', 'Palencia', 'Las Palmas',
+    'Pontevedra', 'La Rioja', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona',
+    'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya',
+    'Zamora', 'Zaragoza'];
+
+  acciones =[ {accion:"Gato", eventos : this.provincias},  {accion:"Perro", eventos : this.provincias},  {accion:"Pollo", eventos : this.provincias}];
+  a  = new FlujoEvento() ;
+
+  selecionado: string;
   casoUsoForm: FormGroup;
   constructor(private fb: FormBuilder,
     private casoServ: CasosUsoService,
@@ -31,10 +50,14 @@ export class AgregarCasoComponent implements OnInit {
 
   ngOnInit() {
     this.casoUsoForm = this.fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(10)]],
-      descripcion: ['', [Validators.required, Validators.minLength(25)]],
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      descripcion: ['', [Validators.required, Validators.minLength(10)]],
       actores: [[], Validators.required]
     });
+
+    this.a.accion = "Pokemon";     
+    this.a.eventos = this.provincias ; 
+    this.acciones.push(this.a); 
   }
   add(chip, nombreLista: string) {
     switch (nombreLista) {
@@ -43,12 +66,6 @@ export class AgregarCasoComponent implements OnInit {
         break;
       case 'postcondiciones':
         this.postcondiciones.push(chip.tag);
-        break;
-      case 'flujos':
-        this.flujos.push(chip.tag);
-        break;
-      case 'eventos':
-        this.eventos.push(chip.tag);
         break;
       case 'requisitosEspeciales':
         this.requisitosEspeciales.push(chip.tag);
@@ -69,12 +86,6 @@ export class AgregarCasoComponent implements OnInit {
       case 'postcondiciones':
         this.postcondiciones = this.precondiciones.filter(item => { return item != chip.tag });
         break;
-      case 'flujos':
-        this.flujos = this.flujos.filter(item => { return item != chip.tag });
-        break;
-      case 'eventos':
-        this.eventos = this.eventos.filter(item => { return item != chip.tag });
-        break;
       case 'requisitosEspeciales':
         this.requisitosEspeciales = this.requisitosEspeciales.filter(item => { return item != chip.tag });
         break;
@@ -90,15 +101,22 @@ export class AgregarCasoComponent implements OnInit {
     const casoUso = new CasoUso();
     casoUso.nombre = this.casoUsoForm.value.nombre;
     casoUso.descripcion = this.casoUsoForm.value.descripcion;
-    casoUso.precondiciones = this.precondiciones; 
-    casoUso.postcondiciones  = this.postcondiciones;
-    casoUso.eventos = this.eventos ; 
-    casoUso.flujos = this.flujos; 
-    casoUso.requisitosEspeciales = this.requisitosEspeciales; 
+    casoUso.precondiciones = this.precondiciones;
+    casoUso.postcondiciones = this.postcondiciones;
+    casoUso.requisitosEspeciales = this.requisitosEspeciales;
     casoUso.actores = this.actores;
     this.casoServ.addCasoUso(casoUso);
   }
   onCancel() {
     this.router.navigate(['/casosDeUso']);
+  }
+
+  openModal() {
+    this.modalActions.emit({ action: "modal", params: ['open'] });
+  }
+
+  closeModal() {
+    this.modalActions.emit({ action: "modal", params: ['close'] });
+    console.log(this.selecionado);
   }
 }
